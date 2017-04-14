aaa8e4895226c1fdbd679481d9cb70eaefb0f23d
Fathuriady <fathuriady@gmail.com>
2017-04-14 18:56:57 +0700

    Koneksi MySQL sukses

---
+++ app/src/main/java/com/rpl/rplku/Config.java
@@ -0,0 +1,30 @@
+package com.rpl.rplku;
+
+/**
+ * Created by Fathur on 4/14/2017.
+ */
+
+public class Config {
+
+    //Address dari script CRUD
+    public static final String URL_ADD = "http://fathuriady.esy.es/rpl/addBarang.php";
+    public static final String URL_GETALL = "http://fathuriady.esy.es/rpl/getAllBarang.php";
+    public static final String URL_GETBARANG = "http://fathuriady.esy.es/rpl/getBarang.php?id=";
+    public static final String URL_UPDATE_BARANG = "http://fathuriady.esy.es/rpl/updateBarang.php";
+    public static final String URL_DELETE_BARANG = "http://fathuriady.esy.es/rpl/deleteBarang.php?id=";
+
+    //Key buat request ke PHP Script
+    public static final String KEY_BARANG_ID = "id";
+    public static final String KEY_BARANG_NAMA = "nama";
+    public static final String KEY_BARANG_JML = "jml";
+
+    //Tag JSON
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA_BARANG = "nama_barang";
+    public static final String TAG_JML_BARANG = "jml_barang";
+
+    //ID barang untuk di pass antar intent
+    public static final String BARANG_ID = "barang_id";
+
+}
--- app/src/main/java/com/rpl/rplku/LresepActivity.java
+++ app/src/main/java/com/rpl/rplku/LresepActivity.java
@@ -1,39 +1,96 @@
 package com.rpl.rplku;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by User on 4/9/2017.
  */
 
 public class LresepActivity extends AppCompatActivity {
-    protected ListView lvItem;
-    protected String[] menu = new String[]{"Soto Betawi","Dodol Garut","Kerak Telor","Es Doger",
-                                "Nasi Goreng","Nasi Uduk"};
+    private ListView lvItem;
+    private String JSON_STRING;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lresep);
+        lvItem = (ListView)findViewById(R.id.listView);
+        getJSON();
+    }
 
-        lvItem = (ListView)findViewById(R.id.list_view);
+    private void showBarang(){
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String,String>> list = new ArrayList<>();
+        try{
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu);
+            for(int i = 0;i<result.length();i++){
+                JSONObject jo = result.getJSONObject(i);
+                String nama = jo.getString(Config.TAG_NAMA_BARANG);
+                String jml = jo.getString(Config.TAG_JML_BARANG);
+
+
+                HashMap<String,String> barang = new HashMap<>();
+                barang.put(Config.TAG_NAMA_BARANG,nama);
+                barang.put(Config.TAG_JML_BARANG,jml);
+
+                list.add(barang);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(LresepActivity.this,list,R.layout.listview,
+                new String[]{Config.TAG_NAMA_BARANG,Config.TAG_JML_BARANG},
+                new int[]{R.id.txtNamaBarang,R.id.txtJmlBarang});
 
         lvItem.setAdapter(adapter);
+    }
 
-        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void,String>{
+            ProgressDialog loading;
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(LresepActivity.this, "Kamu mengklik " + menu[position] , Toast.LENGTH_SHORT).show();
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(LresepActivity.this,"Ambil Data","Tunggu Sebentar");
             }
-        });
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showBarang();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(Config.URL_GETALL);
+                return s;
+            }
+        }
+        GetJSON gj= new GetJSON();
+        gj.execute();
     }
 }
